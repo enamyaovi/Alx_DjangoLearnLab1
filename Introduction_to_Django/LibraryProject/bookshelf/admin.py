e45b3243fcb80576c0303a28d9cc6e0e15b8f0ef
enamyaovi <cephas.tay137@gmail.com>
2024-11-08 14:19:21 +0000

    created a Book Admin Class

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'publication_year')
+    list_filter = ('author', 'title', 'publication_year')
+    search_fields = ('author', 'title', 'publication_year')
+
+admin.site.register(Book,BookAdmin)
